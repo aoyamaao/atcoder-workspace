f525e7278d6ca660af2d73959075c9b40bf9871e
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2025-07-06 06:25:36 +0900

    feat: add execution time display

--- bin/run.sh
+++ bin/run.sh
@@ -4,34 +4,48 @@ CPP_STANDARD="c++20"
 INPUT_FILE="input.txt"
 
 DEBUG_MODE=0
-# 最初の引数が -d であればデバッグモードを有効にする
-if [ "$1" = "-d" ]; then
-  DEBUG_MODE=1
-  shift
-fi
+TIME_MODE=0
+
+while [[ "$1" =~ ^- ]]; do
+  case "$1" in
+    -d)
+      DEBUG_MODE=1
+      shift
+      ;;
+    -s)
+      TIME_MODE=1
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
 
 if [ $# -eq 0 ]; then
-  echo "Usage: $(basename "$0") [-d] <source_file.cpp>"
+  echo "Usage: $(basename "$0") [-d] [-s] <source_file.cpp>"
   exit 1
 fi
 SOURCE_FILE=$1
 
 if [ ! -f "$SOURCE_FILE" ]; then
-  echo "Error: Source file '$SOURCE_FILE' not found in the current directory."
+  echo "Error: Source file '$SOURCE_FILE' not found."
   exit 1
 fi
 
-# パスの解決
 PROJECT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
 if [ $? -ne 0 ]; then
-  echo "Error: Not a git repository. Assuming current directory is project root for -I flag."
+  echo "Error: Not a git repository."
   PROJECT_ROOT="."
 fi
 
 EXECUTABLE_NAME=$(basename "$SOURCE_FILE" .cpp)
 
+if [ "$DEBUG_MODE" -eq 1 ]; then
+  printf "%-12s: %s (version: %s) -> %s\n" "Compiling" "$SOURCE_FILE" "$CPP_STANDARD" "$EXECUTABLE_NAME"
+fi
 
-# コンパイルを実行し、失敗した場合はエラーメッセージを表示して終了
 COMPILE_ERROR=$(clang++ "$SOURCE_FILE" -std="$CPP_STANDARD" -I"$PROJECT_ROOT" -o "$EXECUTABLE_NAME" 2>&1)
 if [ $? -ne 0 ]; then
   echo "--- Compile Failed ---"
@@ -39,18 +53,34 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# デバッグモードが有効な場合のみ、詳細ログを出力
+
+zmodload zsh/datetime
+
+# 実行前の時間を記録
+start_time=$EPOCHREALTIME
+
+# デバッグモードが有効な場合のみ実行ログを出力
 if [ "$DEBUG_MODE" -eq 1 ]; then
-  printf "%-12s: %s (version: %s) -> %s\n" "Compiling" "$SOURCE_FILE" "$CPP_STANDARD" "$EXECUTABLE_NAME"
   printf "%-12s: %s with %s\n" "Running" "$EXECUTABLE_NAME" "$INPUT_FILE"
   printf "%-12s: " "Execution"
 fi
 
-# 後片付け
+# 実行
 ./"$EXECUTABLE_NAME" < "$INPUT_FILE"
-rm "$EXECUTABLE_NAME"
 
-# デバッグモードの最後のログ
+# 実行後の時間を記録
+end_time=$EPOCHREALTIME
+
+
+# 実行可能ファイルを削除
+rm "$EXECUTABLE_NAME"
 if [ "$DEBUG_MODE" -eq 1 ]; then
   printf "%-12s: %s\n" "Cleaned up" "$EXECUTABLE_NAME"
+fi
+
+# 時間計測モードが有効な場合のみ実行時間を表示
+if [ "$TIME_MODE" -eq 1 ]; then
+  duration=$(($end_time - $start_time))
+  duration_ms=$(($duration * 1000))
+  printf "%.0fms\n" $duration_ms
 fi
